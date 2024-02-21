1ac48d0d2c8cad516da524ed743dd14cb9f44285
Eirik Kolås <kolaseirik@gmail.com>
2024-02-21 17:19:47 +0100

    Made select child properties view

--- app/hello-world/src/views/HomeView.vue
+++ app/hello-world/src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <MultiChoiceComponent v-bind:buttonNames="['a', 'b', 'Maiken']" />
     <StartCourse />
     <ChildsInfo />
+    <SelectChildPropertiesView />
   </div>
 </template>
 
@@ -10,13 +11,14 @@
 import MultiChoiceComponent from "@/components/MultiChoiceComponent.vue";
 import StartCourse from "@/components/StartCourse.vue";
 import ChildsInfo from "@/components/ChildsInfo.vue";
+import SelectChildPropertiesView from "./SelectChildPropertiesView.vue";
 export default {
   name: "HomeView",
   components: {
     MultiChoiceComponent,
-    ButtonsComponent,
     StartCourse,
     ChildsInfo,
+    SelectChildPropertiesView,
   },
   props: {
     courseStarted: Boolean,
