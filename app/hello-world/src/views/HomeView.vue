cd93d12a79dd6d4be869b48a0cefef4e25bfc510
Maiken Louise  Brechan <maikebre@stud.ntnu.com>
2024-02-21 15:24:15 +0100

    merged feature/buttonComponent

--- app/hello-world/src/views/HomeView.vue
+++ app/hello-world/src/views/HomeView.vue
@@ -3,21 +3,17 @@
     <MultiChoiceComponent v-bind:buttonNames="['a', 'b', 'Maiken']" />
     <StartCourse />
     <ChildsInfo />
-    <ButtonsComponent text1="Write button text here" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import MultiChoiceComponent from "@/components/MultiChoiceComponent.vue";
-import ButtonsComponent from "@/components/ButtonsComponent.vue";
 import StartCourse from "@/components/StartCourse.vue";
 import ChildsInfo from "@/components/ChildsInfo.vue";
 
 export default {
   name: "HomeView",
   components: {
-    ButtonsComponent,
     MultiChoiceComponent,
     StartCourse,
     ChildsInfo,
