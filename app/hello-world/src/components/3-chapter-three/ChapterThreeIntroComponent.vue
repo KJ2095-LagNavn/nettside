52a1cc610bb0d7fedc37899803b11d472a3f7032
Maiken Louise  Brechan <maikebre@stud.ntnu.com>
2024-03-13 15:29:20 +0100

    started chapter three

---
+++ app/hello-world/src/components/3-chapter-three/ChapterThreeIntroComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="startView">
+    <h1>Kapittel 3: Bevisstgjøring og tiltaksplan for tiden fremover</h1>
+    <p>
+      Hensikten med kapittel 3 er å bevisstgjøre egne bekymringer og opplyse om
+      hvilke tiltak man kan gjøre for å redusere de.
+    </p>
+    <Button v-on:click="startChapter()"> Start Kapittel 3 </Button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    startChapter() {
+      console.log("Start chap");
+    },
+  },
+};
+</script>
+
+<style>
+#startView {
+  padding: 3em;
+  margin: 3em;
+  background-color: #ececec;
+  border-radius: 25px;
+  width: 50%;
+  height: 30%;
+  position: relative;
+}
+</style>
---
+++ app/hello-world/src/components/TextComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <p id="textbox">
+    {{ text }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style>
+#textbox {
+  border-radius: 8px;
+  background-color: bisque;
+}
+</style>
